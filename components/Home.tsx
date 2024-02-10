4f184de249fd0736ee2265c503d949a519a6ad31
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2024-02-10 20:09:56 +0900

    add sqlite tool to Android env

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,3 +1,31 @@
+import { useEffect, useState } from "react";
 import { Text } from "react-native";
+import { openDatabase } from "react-native-sqlite-storage";
 
-export const Home = () => <Text>home</Text>;
+export const Home = () => {
+  const [text, setText] = useState<string>('');
+
+  useEffect(() => {
+    const db = openDatabase({
+      name: 'climbingPlan.sqlite3',
+      createFromLocation: 1,
+      },
+      () => {},
+      e => console.error(e)
+    );
+
+    db.transaction(tx => {
+      const query = 'SELECT * FROM mountains';
+      const params: string[] = [];
+      tx.executeSql(
+        query,
+        params,
+        (tx, res) => setText(JSON.stringify(res.rows.raw())),
+        (tx, e) => console.error(e)
+      );
+    });
+
+  }, []);
+
+  return <Text>{text}</Text>
+};
